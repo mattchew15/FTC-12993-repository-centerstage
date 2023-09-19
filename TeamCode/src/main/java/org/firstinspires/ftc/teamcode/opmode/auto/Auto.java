4f6303cf6131bfa278f9ab4dddfa6df46294ad4c
Lotus <Lotus.i@iCloud.com>
2023-09-19 14:42:18 +1000

    little fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/Auto.java
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
-public class Auto {
+public class Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Teleop.java
@@ -1,7 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class Teleop {
+public class Teleop extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+        while (opModeIsActive()) { // Main loop
+
+        }
+    }
 }
