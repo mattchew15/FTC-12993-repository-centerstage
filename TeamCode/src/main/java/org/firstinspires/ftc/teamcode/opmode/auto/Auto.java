2e4a22107b2b0c78d1a4531e0905c50a4705d3ce
Lotus <Lotus.i@iCloud.com>
2023-09-19 13:13:42 +1000

    tidy up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/Auto.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.opmode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class Auto {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/Teleop.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Teleop {
+}
