55da8092f778093422c1e3b6244b85ad672d3b54
rafa1703 <rafael-fiorelli@uol.com.br>
2023-11-24 01:01:56 +1000

    Kalman filter, i revised the math and and some functions, but still not sure, will go through all the math with Sully tomorrow(actually today because is 1am) but that doesn't matter.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/accessory/kalmanStuff/Kalman.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/accessory/kalmanStuff/Kalman.java
@@ -7,7 +7,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import org.ejml.simple.SimpleMatrix;
 public class Kalman
 {
-    // Todo: find a way to update the transitional matrices and update for a better filter
+    //TODO: Check math
     public SimpleMatrix[] pose;
     private final SimpleMatrix STARTING_COVARIANCE = new SimpleMatrix(new double[][]
     {
@@ -16,11 +16,10 @@ public class Kalman
             {0, 0, Math.toRadians(0.1)} // heading covariance
     });
 
-    // A is something, like transitional matrix for prediction, don't this should always be an identity
+    // A is something, like transitional matrix for prediction, this should always be an identity as the prediction is always current pls update
     private SimpleMatrix A = SimpleMatrix.identity(3);
     // H is the sensor transitional matrix, it indicates the which values we can directly measure with the sensor
-    // so if using april tage identity? and if distance sensor only x?
-    //TODO: make a setter to H
+    // so if using april tage identity? and if distance sensor only x?=
     private SimpleMatrix H = SimpleMatrix.identity(3);
     private SimpleMatrix P; // system covariance
 
@@ -38,30 +37,27 @@ public class Kalman
         this.pose = new SimpleMatrix[]{pose, STARTING_COVARIANCE};
     }
 
-    // Gets the previous state(always this.pose) and the update matrix (should be the difference between previous and current, use RR)
+    // Gets the previous state(always this.pose) and the update matrix should be just RR value; this to work it needs a change (should be the difference between previous and current, use RR)
     public SimpleMatrix[] prediction(SimpleMatrix[] prev, SimpleMatrix update)
     {
-        // use this to update covariance?
-        double prevX = prev[0].get(0,0);
-        double prevY = prev[0].get(1,0);
-        double prevHeading = prev[0].get(2,0);
         SimpleMatrix prevCov = prev[1];
 
-        //Todo make tuning, loss of power of the system
+        //Todo make setter, loss of power of the system, make this global possibly final
+        //Covariance gain for the model
         SimpleMatrix Q = new SimpleMatrix(new double[][]
                 {
                         {0.3, 0, 0},
                         {0, 0.2, 0},
                         {0, 0, 0.9}
                 });
+        // Projects covariance
+        P = (A.mult(prevCov).mult(A.transpose())).plus(Q);
 
-
-        P = A.mult(prevCov).mult(A.transpose()).plus(Q);
         // pass the calculated values to the pose style matrix
         double predX = update.get(0, 0);
         double predY = update.get(1,0);
         double predHeading = angleWrap(update.get(2,0)); // wraps the angle ]-180, 180[
-
+        // Return a matrix on the pose style
         return new SimpleMatrix[]
                 {
                     new SimpleMatrix(new double[][]
@@ -74,7 +70,7 @@ public class Kalman
                 };
     }
 
-    // Gets the prev state (always this.pose) and the matrix that is the observable state
+    // Gets the pred state (always this.pose, take out the argument?) and the matrix that is the observable state
     public SimpleMatrix[] correction(SimpleMatrix[] pred, SimpleMatrix observation)
     {
         SimpleMatrix predCov = pred[1]; // gets the predicted cov index in pose[1]
@@ -86,7 +82,7 @@ public class Kalman
             {observation.get(1, 0)},
             {observation.get(2, 0)}
         });
-        // Todo: tuning this is final value tho after tuning, so make it global?
+        // Process noise for the prediction, can it be ignored? prob not
         SimpleMatrix w = new SimpleMatrix(new double[][]
         {
             {0.4},
@@ -94,30 +90,33 @@ public class Kalman
             {0.0001}
         });
 
-        // don't remember now what this is
+        // Current difference between sensor and pred, given by sensor - (predicted + process noise)
         SimpleMatrix i = z.minus(H.mult(pred[0]).plus(w));;
 
-        // Sensor noise matrix
+        // Sensor noise
         // TODO how to find these values? Michael said he found a sensor noise paper or something like that
-        SimpleMatrix Q = new SimpleMatrix(new double[][]
+        SimpleMatrix R = new SimpleMatrix(new double[][]
                 {
                         {0.20, 0, 0},
                         {0, 0.2, 0},
                         {0, 0, 0, 0.01}
                 });
 
-        // Simplified calculations
-        SimpleMatrix S = H.mult(predCov).mult(H.transpose()).plus(Q);
+        //bottom part of the kalman gain, use invert
+        SimpleMatrix S = H.mult(predCov).mult(H.transpose()).plus(R);
+        // Kalman gain
         SimpleMatrix K = predCov.mult(H.transpose().mult(S.invert()));
-
+        // The actual pos
         SimpleMatrix correct = pred[0].plus(K.mult(i));
         // wraps the angle
         correct.set(2, 0, angleWrap(correct.get(2,0)));
-        SimpleMatrix correctCov = predCov.minus(K.mult(H).mult(predCov));
+        // Updates the covariance
+        // What is I on the formulas?? seems like the identity matrix but not sure anymore, was michael code correct? don't seem like, my formula should be better
+        // SimpleMatrix correctCov = predCov.minus(K.mult(H).mult(predCov));
+        SimpleMatrix correctCov = predCov.mult(SimpleMatrix.identity(3).minus(K).mult(H));
         return new SimpleMatrix[]{correct, correctCov};
     }
 
-    // WTF?
     // Update matrix is the target - previous
     public void update(SimpleMatrix update, SimpleMatrix obs)
     {
@@ -138,7 +137,7 @@ public class Kalman
 
     public SimpleMatrix[] fuse(SimpleMatrix[] prev, SimpleMatrix update, SimpleMatrix obs)
     {
-        SimpleMatrix[] prediction = prediction(prev, update);
+        SimpleMatrix[] prediction = prediction(prev, update); // this is my new pose isn't it?
         return correction(prediction, obs);
     }
 
@@ -150,5 +149,55 @@ public class Kalman
     {
         return pose[1];
     }
-    //TODO: Make a function that gets a pose2d and transforms it in a matrix
+
+    /** This return a 3x1 matrix, use this for the all the matrix and stuff*/
+    public SimpleMatrix poseToMatrix(Pose2d pose2d)
+    {
+        SimpleMatrix matrix = new SimpleMatrix(new double[][]
+                {
+                    {pose2d.getX()},
+                    {pose2d.getY()},
+                    {pose2d.getHeading()}
+                });
+        return matrix;
+    }
+    /** This returns a 3x1 matrix with the first value (x), use this for the sensor stuff*/
+    public SimpleMatrix doubleToMatrix(double val)
+    {
+        SimpleMatrix matrix = new SimpleMatrix(new double[][]
+                {
+                        {val},
+                        {0},
+                        {0}
+                });
+        return matrix;
+    }
+
+    public void setH(double v1, double v2, double v3)
+    {
+        this.H = new SimpleMatrix(new double[][]
+        {
+            {v1, 0, 0},
+            {0, v2, 0},
+            {0, 0, v3}
+    });
+    }
+    public void setH(boolean all)
+    {
+        if (all)
+        {
+            this.H = SimpleMatrix.identity(3);
+        }
+        else
+        {
+            this.H = new SimpleMatrix(new double[][]
+                {
+                        // 3x1 or 3x3?
+                        {1},
+                        {0},
+                        {0}
+                });
+        }
+    }
+
 }
