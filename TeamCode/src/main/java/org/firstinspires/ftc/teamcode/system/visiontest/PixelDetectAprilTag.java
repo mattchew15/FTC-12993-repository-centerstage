2757f4a96ffb40bbc758ac0302e601586b0dce83
rafa1703 <rafael-fiorelli@uol.com.br>
2024-02-04 13:40:39 +1000

    Preload pipeline, to test adaptive in tinzen computer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/visiontest/PixelDetectAprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/visiontest/PixelDetectAprilTag.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode.system.visiontest;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.apriltag.AprilTagDetection;
 
 import java.util.ArrayList;
@@ -14,6 +18,21 @@ public class PixelDetectAprilTag extends AprilTagPipeline
     int position = 0;
     Tag lookTag;
     double[] tagList;
+
+    private Point
+            regionLeftA,
+            regionLeftB,
+            regionRightA,
+            regionRightB;
+
+    private Mat regionLeftLight, regionRightLight;
+
+    protected enum Place
+    {
+        LEFT,
+        RIGHT
+    }
+
 
     public PixelDetectAprilTag(Telemetry telemetry)
         {
@@ -35,6 +54,15 @@ public class PixelDetectAprilTag extends AprilTagPipeline
             }
             tagList = new double[]{tagList[position]};
 
+            regionLeftA = new Point();
+            regionLeftB = new Point();
+
+            regionRightA = new Point();
+            regionRightB = new Point();
+
+            regionLeftLight = new Mat();
+            regionRightLight = new Mat();
+
             //Pose pose = poseFromTrapezoid(detection.corners, cameraMatrix, tagsizeX, tagsizeY);
             //drawAxisMarker(input, tagsizeY/2.0, 6, pose.rvec, pose.tvec, cameraMatrix);
 
@@ -47,40 +75,87 @@ public class PixelDetectAprilTag extends AprilTagPipeline
             super.finalize();
         }
 
-        //@SuppressWarnings("unchecked")
         @Override
         public Mat processFrame(Mat input)
         {
             super.processFrame(input);
-
-            // What the actual fuck i just did?
-            ArrayList<ArrayList<?>> tags = processDetections();
-            ArrayList<Tag> tags1 = (ArrayList<Tag>) tags.get(0);
-            ArrayList<AprilTagDetection> detections1 = (ArrayList<AprilTagDetection>) tags.get(1);
-
-            int i = 0;
-            for(Tag dect: tags1)
+            ArrayList<AprilTagDetection> detections = getDetectionsUpdate();
+            Place place;
+            Point middlePoint;
+            if (detections != null && detections.size() > 0)
             {
-                if (dect.id == tagList[0])
+                for (AprilTagDetection detection : detections)
                 {
-                    lookTag = dect;
-                    break;
-                }
-                else
-                {
-                    i++;
+                    if (detection.id == tagList[0])
+                    {
+                        middlePoint = detection.center;
+
+                        int leftX = Integer.MAX_VALUE;
+                        int rightX = Integer.MIN_VALUE;
+                        int topY = Integer.MIN_VALUE;
+                        int bottomY = Integer.MAX_VALUE;
+
+                        for (Point point : detection.corners) {
+                            if (point.x < leftX) leftX = (int) point.x;
+                            if (point.x > rightX) rightX = (int) point.x;
+                            if (point.y > topY) topY = (int) point.y;
+                            if (point.y < bottomY) bottomY = (int) point.y;
+                        }
+
+                        int tagCenterX = (int) detection.center.x;
+                        int tagCenterY = (int) detection.center.y;
+
+                        int tagWidth = rightX - leftX;
+                        int tagHeight = topY - bottomY;
+
+                        int inclusionZoneWidth = (int) (tagWidth * 1.5);
+                        int inclusionZoneHeight = (int) (tagHeight * 1.5);
+
+                        int exclusionZoneWidth = (int) (tagWidth * 0.28);
+                        int exclusionZoneHeight = (int) (tagHeight * 0.28);
+
+                        Rect leftInclusionZone = new Rect(tagCenterX - inclusionZoneWidth, tagCenterY - 110, inclusionZoneWidth, inclusionZoneHeight);
+                        Rect rightInclusionZone = new Rect(tagCenterX, tagCenterY - 110, inclusionZoneWidth, inclusionZoneHeight);
+
+                        Rect leftExclusionZone = new Rect(tagCenterX - (int) (inclusionZoneWidth * 0.64), tagCenterY - 90, exclusionZoneWidth, exclusionZoneHeight);
+                        Rect rightExclusionZone = new Rect(tagCenterX + (int) (inclusionZoneWidth * 0.28), tagCenterY - 90, exclusionZoneWidth, exclusionZoneHeight);
+
+
+                        //regionLeftA = new Point(middlePoint.x, middlePoint.y - 200);
+                        //regionLeftB = new Point(middlePoint.x - 100, middlePoint.y - 100);
+
+                        //regionRightA = new Point(middlePoint.x, middlePoint.y - 200);
+                        //regionRightB = new Point(middlePoint.x + 100, middlePoint.y - 100);
+
+                        // Create area
+                        regionLeftLight = input.submat(leftInclusionZone);
+                        regionRightLight = input.submat(rightInclusionZone);
+
+
+                        double leftAvg = Core.mean(regionLeftLight).val[0];
+                        double rightAvg = Core.mean(regionRightLight).val[0];
+
+                        place = leftAvg > rightAvg ? Place.LEFT : Place.RIGHT;
+
+
+                        Imgproc.rectangle(input, leftInclusionZone, new Scalar(0, 0, 255), 2);
+                        Imgproc.rectangle(input, rightInclusionZone, new Scalar(0, 0, 255), 2);
+
+                        if (place == Place.LEFT)
+                        {
+                            Imgproc.rectangle(input, regionLeftA, regionLeftB, new Scalar(255, 0, 0), -1);
+                        } else
+                        {
+                            Imgproc.rectangle(input, regionRightA, regionRightB, new Scalar(255, 0, 0), -1);
+                        }
+
+                        telemetry.addData("Place", place);
+                        break;
+                        // no need to iterate if you found it
+                    }
+
                 }
             }
-            AprilTagDetection myDetection; // wrong scope, fine by now
-            if (detections1.size() > 0)
-            {
-                myDetection = detections1.get(0);
-                Pose pose = poseFromTrapezoid(myDetection.corners, cameraMatrix, tagsizeX, tagsizeY);
-                Point point = getMiddlePoint(tagsizeY / 2.0, pose.rvec,pose.tvec,cameraMatrix);
-                telemetry.addData("Ponto", point);
-            }
-
-
             telemetry.update();
             return input;
         }
