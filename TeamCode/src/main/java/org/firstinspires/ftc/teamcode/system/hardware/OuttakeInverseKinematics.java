ee62be8a83e32eab0f5ad7993d3aae5c91c4e8a2
L0TUSflower <145341598+L0TUSflower@users.noreply.github.com>
2024-03-27 16:04:15 +1000

    ot kin fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/hardware/OuttakeInverseKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/hardware/OuttakeInverseKinematics.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode.system.hardware;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import static org.firstinspires.ftc.teamcode.system.hardware.OuttakeSubsystem.RAIL_RANGE;
 
 public class OuttakeInverseKinematics {
-    OuttakeSubsystem outtakeSubsystem;
     public final double
             offset = 3,
             slideLength = 11.811;
@@ -52,16 +50,10 @@ public class OuttakeInverseKinematics {
         }
     }
 
-    // heightStart and railStart needs to be cached. Alternatively use servo.getposition into the parameter
-    public double railEndInch(double heightStart, double heightEnd, double railStart, double robotAngle) {
+    // rail start needs to be cached. Alternatively use servo.getposition into the parameter
+    public double railEnd(double heightStart, double heightEnd, double railStart, double robotAngle) {
         robotAngleRad = Math.toRadians(robotAngle);
         varC = (heightEnd - heightStart) / (Math.sqrt(3) * Math.cos(robotAngleRad));
         return varC * Math.sin(robotAngleRad) + railStart;
     }
-
-    /*
-    public double railEndTicks(double heightStart, double heightEnd, double railStart, double robotAngle) {
-        return outtakeSubsystem.railInchesToTicks(railEndInch(heightStart, heightEnd, railStart, robotAngle));
-    }
-     */
 }
