d23413d5e5b252b4e754dfa2e9f68116616c4d33
rafa1703 <rafael-fiorelli@uol.com.br>
2024-03-29 11:23:01 +1000

    RR pid updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0117;
-    public static double kA =  0.004;
-    public static double kStatic = 0.035;
+    public static double kV = 0.012;
+    public static double kA =  0.0036;
+    public static double kStatic = 0.01;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
