08d071b783979b9ac583b5e8998f25ffbf818e2e
L0TUSflower <145341598+L0TUSflower@users.noreply.github.com>
2024-04-09 21:56:24 -0500

    preload math fixy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/vision/YCrCbRedTeamPropDetectorPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/system/vision/YCrCbRedTeamPropDetectorPipeline.java
@@ -94,7 +94,7 @@ public class YCrCbRedTeamPropDetectorPipeline extends OpenCvPipeline {
         int max = Math.max(avg1, Math.max(avg2, avg3));
 
         if (max == avg1) {
-            teamPropLocation = 1;
+            teamPropLocation = 3;
             telemetry.addLine("Left");
             Imgproc.rectangle(input, region1A, region1B, green, -1);
         } else if (max == avg2) {
@@ -102,7 +102,7 @@ public class YCrCbRedTeamPropDetectorPipeline extends OpenCvPipeline {
             telemetry.addLine("Center");
             Imgproc.rectangle(input, region2A, region2B, green, -1);
         } else {
-            teamPropLocation = 3;
+            teamPropLocation = 1;
             telemetry.addLine("Right");
             Imgproc.rectangle(input, region3A, region3B, green, -1);
         }
